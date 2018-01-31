dc6cde22fb871975b8110ec1856cce7d5b0fd9a9
Chris <user@example.com>
2018-01-30 18:47:19 -0800

    Add no-images message
    This adds a "no images" warning after clearing the app.
    This explains the empty screen and prompts the user with actions to
    add images.
    
    This also adds some basic animations.

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ class App extends Component {
     super(props);
     this.state = {
       loading: false,
-      images: [],
+      images: []
     };
     this.searchImages = this.searchImages.bind(this);
+    this.clearImages = this.clearImages.bind(this);
+    this.getImages = this.getImages.bind(this);
   }
 
   componentDidMount(){
@@ -23,45 +25,96 @@ class App extends Component {
   }
 
   searchImages(term){
-    this.setState({loading: true});
+    this.setState({
+      loading: true
+    });
     fetch(`http://api.giphy.com/v1/gifs/search?api_key=IK1r2Tz4gU9FgDltR7LlJhzaUKvZvu1C&q=${term}`)
       .then((response) => response.json())
       .then((result) => this.setImages(result.data));
   }
 
   getImages(){
-    this.setState({loading: true});
+    this.setState({
+      loading: true
+    });
     fetch("http://api.giphy.com/v1/gifs/trending?api_key=IK1r2Tz4gU9FgDltR7LlJhzaUKvZvu1C")
       .then((response) => response.json())
       .then((result) => this.setImages(result.data));
   }
 
   clearImages(){
-    this.setState({images: []});
+    this.setState({images: [], cleared: true});
   }
 
   render() {
     return (
       <div className="App">
-        <div className="titlebar clearfix">
-          <h1>GIPHY trending explorer</h1>
-          <div className="clear">
-            <button onClick={() => this.clearImages()}>Clear</button>
-          </div>
-          <div className="refresh">
-            <button onClick={() => this.getImages()}>Refresh</button>
-          </div>
-          <Search queryFn={this.searchImages} />
-        </div>
-        <div className="tiled">
-          {this.state.images.map((image, id) => {
-            return <Image key={id} image={image} />;
-          })}
+        <TitleBar
+          getImages={this.getImages}
+          clearImages={this.clearImages}
+          searchImages={this.searchImages} />
+        <div className="content">
+          {this.state.loading &&
+            <div className="loading"></div>
+          }
+          {!this.state.loading &&
+            <div className="tiled">
+              {this.state.images.length > 0 &&
+                this.state.images.map((image, id) => {
+                  return <Image key={id} image={image} />;
+                })
+              }
+              {this.state.images.length <= 0 &&
+                <div className="no_images">
+                  <h5>No GIFs to show</h5>
+                  <div>Use the
+                    <Search queryFn={this.searchImages} />
+                    or press
+                    <TrendingButton getImages={this.getImages} />
+                  </div>
+                </div>
+              }
+            </div>
+          }
         </div>
       </div>
     );
   }
 }
+
+class TitleBar extends Component {
+  render(){
+    return (
+      <div className="titlebar clearfix">
+        <h1>
+          GIPHY trending explorer
+
+        </h1>
+        <div className="clear">
+          <ClearButton clearImages={this.props.clearImages} />
+        </div>
+        <div className="refresh">
+          <TrendingButton getImages={this.props.getImages} />
+        </div>
+        <Search queryFn={this.props.searchImages} showButton={true} />
+      </div>
+    );
+  }
+}
+
+class ClearButton extends Component {
+  render(){
+    return (<button onClick={this.props.clearImages}>Clear all</button>);
+  }
+}
+
+class TrendingButton extends Component {
+  render(){
+    return (<button onClick={this.props.getImages}>View trending</button>);
+  }
+}
+
+
 
 class Search extends Component {
   constructor(props){
@@ -73,13 +126,15 @@ class Search extends Component {
   render(){
     return (
       <div className="search">
-        <input type="text"
-          name="search"
-          placeholder="search"
-          onChange={(e) => this.setState({term: e.target.value})} />
-        <button onClick={() => this.props.queryFn(this.state.term)}>
-          Search
-        </button>
+        <form onSubmit={(e) => {e.preventDefault(); this.props.queryFn(this.state.term)}}>
+          <input type="text"
+            name="search"
+            placeholder="search"
+            onChange={(e) => this.setState({term: e.target.value})} />
+          {this.props.showButton &&
+            <input type="submit" value="Search" />
+          }
+        </form>
       </div>
     );
   }
