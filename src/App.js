d81abb7f96e72ca1aa4df975a480da791a162cf3
Chris <user@example.com>
2018-01-30 17:26:34 -0800

    Add basic search functionality
    This commit adds a basic search input that will submit the query to
    giphy and show the results.
    
    This adds new styles to put the search bar at the top of the page
    in the titlebar area.
    
    This modifies the App to abstract the adding of images to a new function
    setImages(), and uses this in a new function searchImages(), which acts
    like getImages() but takes in a query.
    
    This adds a search component which shows a search bar and calls a search
    function passed down from the app.

--- src/App.js
+++ src/App.js
@@ -4,24 +4,57 @@ import './App.css';
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = {};
-    this.getImages = this.getImages.bind(this);
+    this.state = {
+      loading: false,
+      images: [],
+    };
+    this.searchImages = this.searchImages.bind(this);
+  }
+
+  componentDidMount(){
     this.getImages();
   }
 
+  setImages(images){
+    this.setState({
+      loading: false,
+      images: images
+    })
+  }
+
+  searchImages(term){
+    this.setState({loading: true});
+    fetch(`http://api.giphy.com/v1/gifs/search?api_key=IK1r2Tz4gU9FgDltR7LlJhzaUKvZvu1C&q=${term}`)
+      .then((response) => response.json())
+      .then((result) => this.setImages(result.data));
+  }
+
   getImages(){
+    this.setState({loading: true});
     fetch("http://api.giphy.com/v1/gifs/trending?api_key=IK1r2Tz4gU9FgDltR7LlJhzaUKvZvu1C")
       .then((response) => response.json())
-      .then((x) => this.setState({images: x.data}));
+      .then((result) => this.setImages(result.data));
+  }
+
+  clearImages(){
+    this.setState({images: []});
   }
 
   render() {
-    const images = this.state.images ? this.state.images : [];
     return (
       <div className="App">
-        <h1>GIPHY trending explorer</h1>
+        <div className="titlebar clearfix">
+          <h1>GIPHY trending explorer</h1>
+          <div className="clear">
+            <button onClick={() => this.clearImages()}>Clear</button>
+          </div>
+          <div className="refresh">
+            <button onClick={() => this.getImages()}>Refresh</button>
+          </div>
+          <Search queryFn={this.searchImages} />
+        </div>
         <div className="tiled">
-          {images.map((image, id) => {
+          {this.state.images.map((image, id) => {
             return <Image key={id} image={image} />;
           })}
         </div>
@@ -30,7 +63,27 @@ class App extends Component {
   }
 }
 
-export default App;
+class Search extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      term: ""
+    };
+  }
+  render(){
+    return (
+      <div className="search">
+        <input type="text"
+          name="search"
+          placeholder="search"
+          onChange={(e) => this.setState({term: e.target.value})} />
+        <button onClick={() => this.props.queryFn(this.state.term)}>
+          Search
+        </button>
+      </div>
+    );
+  }
+}
 
 class Image extends Component {
   render() {
@@ -44,3 +97,6 @@ class Image extends Component {
     );
   }
 }
+
+
+export default App;
